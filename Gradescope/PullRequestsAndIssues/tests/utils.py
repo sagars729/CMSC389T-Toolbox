84918a70b7e9e25ec2eb819bd8257ef954a5bae6
Sagar Saxena <ssaxena1@umd.edu>
2022-09-26 11:24:20 -0400

    Add Pull Requests and Issues Project Autograder (#17)
    ## Summary of Changes
    In this PR, I have included a new autograder for the `Pull Requests and
    Issues` project. This includes updates to include a GitHub token and
    Student roster to ensure that all tests run and validate successfully.
    
    ## Motivation
    The Pull Requests and Issues project allows students to get early
    feedback on whether they are correctly creating pull requests and issues
    on GitHub. We can automate this process to give students quicker
    feedback on their assignment.
    
    Closes https://github.com/sagars729/CMSC389T-Toolbox/issues/14 
    
    ## Testing
    This autograder was tested with sample submissions and deployed for
    students to use in Fall 2022. Students were able to successfully get
    their assignments accurately graded by using this system.

---
+++ Gradescope/PullRequestsAndIssues/tests/utils.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import base64
+
+GH_TOKEN = open(".token").readline().strip()
+HEADERS = {
+  'Authorization': f'Bearer {GH_TOKEN}'
+}
+
+def request_github(path):
+    return requests.get(f"https://api.github.com/{path}", headers=HEADERS)
+
+def request_graphql(data={}):
+    return requests.post(f"https://api.github.com/graphql", headers=HEADERS,
+                         data=json.dumps(data))
+
+def response_to_str(response):
+    return base64.base64.decode(response.json()['content']).decode()
+
