b5a56e7812f4ed8cc3c707ffe36d7daad4794532
Sagar Saxena <ssaxena1@umd.edu>
2022-11-28 13:23:41 -0500

    add p4 gradescope

---
+++ Gradescope/PacManCI/tests/test_issues.py
@@ -0,0 +1,99 @@
+import unittest
+from gradescope_utils.autograder_utils.decorators import weight, number
+
+from utils import request_github, request_graphql, read_submission
+import base64
+import json
+
+
+def get_project_id(team):
+  projects = request_graphql({'query':
+      """
+      query {
+        organization(login: \"cmsc389T-fall22\") {
+          projectsV2(first: 50) {
+            nodes {
+              id
+              title
+            }
+          }
+        }
+      }
+      """
+  }).json()['data']['organization']['projectsV2']['nodes']
+
+  projects = [project for project in projects
+              if project['title'].lower() == f'{team} PacMan CI Backlog'.lower()]
+
+  assert len(projects) > 0, f'Project "{team} PacMan CI Backlog" Not Found'
+  return projects[0]['id']
+
+
+def load_project_cards(project_id):
+  project_cards = request_graphql({'query':
+      """
+      query {
+        node(id: \"%s\") {
+          ... on ProjectV2 {
+            items (first: 100) {
+              nodes {
+                content {
+                  ... on Issue {
+                    title
+                    body
+                    assignees(first: 1) {
+                      nodes {
+                        login
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      """ % (project_id)
+  }).json()['data']['node']['items']['nodes']
+
+  # restructure cards
+  project_cards = [{
+    "assignee": card['content']['assignees']['nodes'][0]['login'],
+    "title": card['content']['title'],
+    "body": card['content']['body']
+  } for card in project_cards
+    if 'assignees' in card['content'] and
+    len(card['content']['assignees']['nodes']) > 0]
+
+  assert len(project_cards) > 0, f'No cards found for project "{project_id}"'
+
+  return project_cards
+
+class TestIssues(unittest.TestCase):
+
+    def setUp(self):
+      self.gh_username, self.gh_team, _ = read_submission()
+      self.project_id = get_project_id(self.gh_team)
+      self.cards = load_project_cards(self.project_id)
+
+      self.cards = [card for card in self.cards
+                    if card['assignee'] == self.gh_username]
+      self.card_titles = [card['title'].lower() for card in self.cards]
+
+    @weight(5)
+    @number("4.1")
+    def test_assigned_to_actions_issue_card(self):
+      "Created a Actions Issue Card and Assigned Themselves"
+      self.assertIn("actions", ' '.join(self.card_titles))
+
+    @weight(5)
+    @number("4.2")
+    def test_assigned_to_sabotage_issue_card(self):
+      "Created a Sabotage Issue Card and Assigned Themselves"
+      self.assertIn("sabotage", ' '.join(self.card_titles))
+
+    @weight(5)
+    @number("4.3")
+    def test_assigned_to_fix_issue_card(self):
+      "Created a Fix Issue Card and Assigned Themselves"
+      self.assertIn("fix", ' '.join(self.card_titles))
